ac57db1fa77219efc22058dc1c4d893bed59391d
paris_ngc <paris@ngc.com>
2023-03-06 11:34:30 -0600

    -Completed LR_value

--- lvalue_rvalue/lvalue_rvalue.cc
+++ lvalue_rvalue/lvalue_rvalue.cc
@@ -1,35 +1,34 @@
 #include <iostream>
-
 using namespace std;
 
-void func (int & x)
-{ cout << "func(int & x): lvalue ref" << endl; }
+void func (int & x){ cout << "func(int & x): lvalue ref" << endl; }
 
 #if 1
-void func (int const & x)
-{ cout << "func(int const & x): const lvalue ref" << endl; }
+void func (int const & x){ cout << "func(int const & x): const lvalue ref" << endl; }
 #endif
 
-void func (int && x)
-{ cout << "func(int && x) rvalue ref" << endl; }
+void func (int && x){ cout << "func(int && x) rvalue ref" << endl; }
 
 #if 1
-void func (int const && x)
-{ cout << "func(int const && x) const rvalue ref" << endl; }
+void func (int const && x){ cout << "func(int const && x) const rvalue ref" << endl; }
+
 #endif
 
-int main ()
-{
+int main (){
   func(5);
   func(2+3);
   func(int{5});
-
+  
+  cout << endl; 
   int x = 5;
+  
   func(x);
   func(x/2);
   func(move(x));
 
+  cout << endl; 
   const int y = 5;
+ 
   func(y);
   func(y/2);
   func(move(y));
